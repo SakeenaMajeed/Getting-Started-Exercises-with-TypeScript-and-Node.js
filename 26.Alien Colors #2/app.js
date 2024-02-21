5c3414199489d415a5547d7e37188afe1ebfec64
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 01:21:30 +0500

    Add files via upload

---
+++ 26.Alien Colors #2/app.js
@@ -0,0 +1,12 @@
+"use strict";
+// Define the variable alienColor and assign a color to it
+let alienColor = "green"; // or any other color like "red", "blue", etc.
+// Check if the alien's color is green
+if (alienColor === "green") {
+    // If the alien's color is green, print a statement that the player just earned 5 points
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else {
+    // If the alien's color is not green, print a statement that the player just earned 10 points
+    console.log("Congratulations! You just earned 10 points.");
+}
