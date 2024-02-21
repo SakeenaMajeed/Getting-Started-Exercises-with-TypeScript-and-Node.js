157f414b5562d762610ba3f6352d1f256831284a
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 01:19:07 +0500

    Add files via upload

---
+++ 25.Alien Colors #1/app.js
@@ -0,0 +1,11 @@
+"use strict";
+//Version 1: Passing the if test
+let Alien_color = 'green';
+if (Alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+//version 2: Failing the if test
+let alien_color = 'red';
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
---
+++ 25.Alien Colors #1/app.ts
@@ -0,0 +1,16 @@
+
+
+//Version 1: Passing the if test
+
+let Alien_color: string = 'green';
+
+if (Alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+//version 2: Failing the if test
+
+let alien_color: string = 'red';
+
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
