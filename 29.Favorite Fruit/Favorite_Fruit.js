d336114e9234f5202c0bcb01d4d4e36e96c5094b
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 01:28:34 +0500

    Add files via upload

---
+++ 29.Favorite Fruit/Favorite_Fruit.js
@@ -0,0 +1,23 @@
+"use strict";
+// Array of favorite fruits
+const favorite_fruits = ["banana", "apple", "strawberry"];
+// Check if "banana" is in the array
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like bananas!");
+}
+// Check if "apple" is in the array
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apples!");
+}
+// Check if "strawberry" is in the array
+if (favorite_fruits.includes("strawberry")) {
+    console.log("You really like strawberries!");
+}
+// Check if "orange" is in the array
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like oranges!");
+}
+// Check if "grape" is in the array
+if (favorite_fruits.includes("grape")) {
+    console.log("You really like grapes!");
+}
