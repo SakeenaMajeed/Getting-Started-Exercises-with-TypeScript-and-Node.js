c416c3b4bc0b86a34b28a00cd03e976ec0c21bdf
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 00:59:03 +0500

    Add files via upload

---
+++ 14.Guest List/Guest_List.js
@@ -0,0 +1,11 @@
+"use strict";
+// Define the guest list
+const guestList = ["Sukaina", "Mariyam", "Alishba"];
+// Function to send dinner invitations
+function sendInvitation(guest) {
+    console.log(`Dear ${guest},\nYou are cordially invited to dinner at my place. It would be an honor to have your presence.\n\nBest regards,\n[Sakeena]`);
+}
+// Loop through the guest list and send invitations
+guestList.forEach((guest) => {
+    sendInvitation(guest);
+});
