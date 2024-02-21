1a80f23a859ea636e34142f8c6d2eed4e583882b
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 01:52:16 +0500

    Add files via upload

---
+++ 39.City Names/City_Names.js
@@ -0,0 +1,8 @@
+"use strict";
+function city_country(city, country) {
+    return `${city}, ${country}`;
+}
+// Test cases
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
---
+++ 39.City Names/City_Names.ts
@@ -0,0 +1,8 @@
+function city_country(city: string, country: string): string {
+    return `${city}, ${country}`;
+}
+
+// Test cases
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
