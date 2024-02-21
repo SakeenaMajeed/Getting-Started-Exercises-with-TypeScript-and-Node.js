a3c51ceb9c8b217550bd9ff26e18efb520970e7d
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 00:44:56 +0500

    Add files via upload

---
+++ 9.Favorite Number/Favorite_Numbe.js
@@ -0,0 +1,7 @@
+"use strict";
+// Store your favorite number in a variable
+const favoriteNumber = 2;
+// Create a message that reveals your favorite number
+const message = `My favorite number is ` + favoriteNumber;
+// Print the message
+console.log(message);
---
+++ 9.Favorite Number/Favorite_Numbe.ts
@@ -0,0 +1,12 @@
+
+// Store your favorite number in a variable
+
+const favoriteNumber: number = 2;
+
+// Create a message that reveals your favorite number
+
+const message: string = `My favorite number is `+ favoriteNumber ;
+
+// Print the message
+
+console.log(message);
