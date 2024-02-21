c5725e8ce299a691a00c23765b2ce2de3bd5b150
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-21 22:52:49 +0500

    Add files via upload
    My 1st  Assignment!

---
+++ 2. Personal Message/'personal'-message.js
@@ -0,0 +1,8 @@
+// Store the person's name in a variable
+var personName = "sakeena";
+// Construct the message using a template literal with extra enthusiasm
+var message = "Greetings, ".concat(personName, "! Are you ready to embark on an exciting journey into the world of Python today?");
+// Print the message with some extra flair
+console.log("== MESSAGE START ==");
+console.log(message.toUpperCase());
+console.log("== MESSAGE END ==");
---
+++ 2. Personal Message/'personal'-message.ts
@@ -0,0 +1,8 @@
+// Store the person's name in a variable
+const personName: string = "sakeena";
+// Construct the message using a template literal with extra enthusiasm
+const message: string = `Greetings, ${personName}! Are you ready to embark on an exciting journey into the world of Python today?`;
+// Print the message with some extra flair
+console.log("== MESSAGE START ==");
+console.log(message.toUpperCase());
+console.log("== MESSAGE END ==");
