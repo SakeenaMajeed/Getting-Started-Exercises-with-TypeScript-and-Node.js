b2655ecf6018b8cf4af7667f93edaf77a85cf1a9
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 00:56:58 +0500

    Add files via upload

---
+++ 13.Your Own Array/Your_Own_Array.js
@@ -0,0 +1,7 @@
+"use strict";
+// Define an array of cars
+const cars = ["Lamborghini", "Honda Civic", "Toyota Supra", "Ferrari", "BMW 3 Series"];
+// Iterate over the array and print statements
+cars.forEach(car => {
+    console.log(`I would like to own a ${car}.`);
+});
