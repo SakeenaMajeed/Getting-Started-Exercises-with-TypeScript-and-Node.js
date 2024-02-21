b23ad3042caaa3b3ad98a3d9810bdfcde6d6aa3c
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 00:52:12 +0500

    Add files via upload

---
+++ 11.Names/Name.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ["Manahil", "Mariyam", "Alishba", "Nilofar", "Eman", "Majeed", "Ali", "Hamza", "Qasim"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11.Names/Name.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["Manahil", "Mariyam", "Alishba", "Nilofar", "Eman", "Majeed", "Ali", "Hamza", "Qasim"];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
