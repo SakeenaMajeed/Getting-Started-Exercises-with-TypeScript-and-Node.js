5c0b9339265c883faf5058407c109d3abd840ef6
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-21 23:00:06 +0500

    Add files via upload

---
+++ 5.Famous Quote 2/'Famous'-Quote-2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, "imagination is more important than knowledge."`;
+console.log(message);
---
+++ 5.Famous Quote 2/'Famous'-Quote-2.ts
@@ -0,0 +1,4 @@
+let famous_person: string = "Albert Einstein";
+let message: string =  `${famous_person} once said, "imagination is more important than knowledge."`
+console.log(message);
+
