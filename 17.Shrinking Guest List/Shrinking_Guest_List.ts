e59ef3af3c2af4aca688e128c240a43a165e0bb1
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 01:04:51 +0500

    Add files via upload

---
+++ 17.Shrinking Guest List/Shrinking_Guest_List.ts
@@ -0,0 +1,18 @@
+
+let guests: string[] = ["Mariyam", "Manahil", "Eman", "Noor", "Alishba"];
+
+console.log("Unfortunately, the dinner table won't arrive in time. You can only invite two people for dinner.");
+
+while (guests.length > 2) {
+    const removedGuest = guests.pop();
+    if (removedGuest) {
+        console.log(`Sorry, ${removedGuest}, but I can't invite you to dinner.`);
+    }
+}
+for (const remainingGuest of guests) {
+    console.log(`Hey ${remainingGuest}, you're still invited to dinner!`);
+}
+guests.pop();
+guests.pop();
+
+console.log("Final guest list:", guests);
