d7645d995ae809d9e3ccbbfc0bcc77395315b642
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 00:39:00 +0500

    Add files via upload
    My 6th Assignment !

---
+++ 6.Stripping Names/Stripping_Names.js
@@ -0,0 +1,7 @@
+"use strict";
+let nameWithWhitespace = "\t\n    sakeena    \t\n";
+console.log("Name with whitespace:");
+console.log(nameWithWhitespace);
+let strippedName = nameWithWhitespace.trim();
+console.log("\nStripped name:");
+console.log(strippedName);
---
+++ 6.Stripping Names/Stripping_Names.ts
@@ -0,0 +1,9 @@
+let nameWithWhitespace: string = "\t\n    sakeena    \t\n";
+
+console.log("Name with whitespace:");
+console.log(nameWithWhitespace);
+
+let strippedName: string = nameWithWhitespace.trim();
+
+console.log("\nStripped name:");
+console.log(strippedName);
