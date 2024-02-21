df387ca61357ab9b5e7ca326e77e3f4856f4da1f
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 00:43:13 +0500

    Add files via upload

---
+++ 8. Number Eight 2/Number_Eight_2.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log(5 + 3);
+console.log("________________________________");
+console.log(4 * 2);
+console.log("________________________________");
+console.log(10 - 2);
+console.log("________________________________");
+console.log(16 / 2);
+console.log("________________________________");
