9d186901da85078e152f7be3269fdd6c3b4c419b
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 01:07:37 +0500

    Add files via upload

---
+++ 19.Dinner Guests/Dinner_Guests.js
@@ -0,0 +1,10 @@
+"use strict";
+// Define the guests
+let dinnerGuests = ["Alishba", "Manahil", "Mariyam", "Eman",];
+//print the list of dinner guests
+console.log("list of the dinner guests:");
+for (let guest of dinnerGuests) {
+    console.log(guest);
+}
+//print the number of dinner guests
+console.log(`You are inviting ${dinnerGuests.length} people to dinner.`);
---
+++ 19.Dinner Guests/Dinner_Guests.ts
@@ -0,0 +1,11 @@
+// Define the guests
+let dinnerGuests: string[] = ["Alishba", "Manahil", "Mariyam", "Eman",];
+
+//print the list of dinner guests
+console.log("list of the dinner guests:");
+
+for (let guest of dinnerGuests) {
+     console.log(guest);
+ }
+ //print the number of dinner guests
+ console.log(`You are inviting ${dinnerGuests.length} people to dinner.`)
