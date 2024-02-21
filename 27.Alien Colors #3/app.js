0b8237be22810e70d82eae244c16961bffbb415e
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 01:23:00 +0500

    Add files via upload

---
+++ 27.Alien Colors #3/app.js
@@ -0,0 +1,34 @@
+"use strict";
+//Version 1:
+let AlienColor = "green";
+if (AlienColor === "green") {
+    console.log("You earned 5 points!");
+}
+else if (AlienColor === "yellow") {
+    console.log("You earned 10 points!");
+}
+else if (AlienColor === "red") {
+    console.log("You earned 15 points!");
+}
+//Version 2:
+let alienColor = "yellow";
+if (alienColor === "green") {
+    console.log("You earned 5 points!");
+}
+else if (alienColor === "yellow") {
+    console.log("You earned 10 points!");
+}
+else if (alienColor === "red") {
+    console.log("You earned 15 points!");
+}
+//Version 3:
+let aliencolor = "red";
+if (alienColor === "green") {
+    console.log("You earned 5 points!");
+}
+else if (alienColor === "yellow") {
+    console.log("You earned 10 points!");
+}
+else if (alienColor === "red") {
+    console.log("You earned 15 points!");
+}
---
+++ 27.Alien Colors #3/app.ts
@@ -0,0 +1,33 @@
+//Version 1:
+let AlienColor: string = "green";
+
+if (AlienColor === "green") {
+    console.log("You earned 5 points!");
+} else if (AlienColor === "yellow") {
+    console.log("You earned 10 points!");
+} else if (AlienColor === "red") {
+    console.log("You earned 15 points!");
+}
+
+//Version 2:
+
+let alienColor: string = "yellow";
+
+if (alienColor === "green") {
+    console.log("You earned 5 points!");
+} else if (alienColor === "yellow") {
+    console.log("You earned 10 points!");
+} else if (alienColor === "red") {
+    console.log("You earned 15 points!");
+}
+
+//Version 3:
+let aliencolor: string = "red";
+
+if (alienColor === "green") {
+    console.log("You earned 5 points!");
+} else if (alienColor === "yellow") {
+    console.log("You earned 10 points!");
+} else if (alienColor === "red") {
+    console.log("You earned 15 points!");
+}
