828b816efa05573fa6d0d41a08fed9ee697252b2
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-21 22:59:01 +0500

    Add files via upload

---
+++ 4.Famous Quote/'famous'-quote.js
@@ -0,0 +1,9 @@
+"use strict";
+// Storing the famous quote in a variable
+let famous_quote = "Imagination is more important than knowledge.";
+// Storing the author's name in a variable
+let author_name = "Albert Einstein";
+// Composing the message
+let message = ` ${author_name} once said, "${famous_quote}"`;
+// Printing the message
+console.log(message);
---
+++ 4.Famous Quote/'famous'-quote.ts
@@ -0,0 +1,13 @@
+// Storing the famous quote in a variable
+let famous_quote: string = "Imagination is more important than knowledge.";
+
+// Storing the author's name in a variable
+let author_name: string = "Albert Einstein";
+
+// Composing the message
+let message: string = ` ${author_name} once said, "${famous_quote}"`;
+
+// Printing the message
+console.log(message);
+
+
