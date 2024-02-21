8e20b9cad39c8cda7a177aa7eed4ee92155a2f5b
sakeena majeed <160149270+SakeenaMajeed@users.noreply.github.com>
2024-02-22 01:15:16 +0500

    Add files via upload

---
+++ 23.Conditional Tests/Conditional_Tests.js
@@ -0,0 +1,32 @@
+"use strict";
+let car = 'subaru';
+// Test 1
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru'); // True
+// Test 2
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car == 'toyota'); // False
+// Test 3
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru'); // True
+// Test 4
+console.log("Is car === 'Subaru'? I predict False.");
+console.log(car === 'Subaru'); // False
+// Test 5
+console.log("Is car !== 'honda'? I predict True.");
+console.log(car !== 'honda'); // True
+// Test 6
+console.log("Is car !== 'subaru'? I predict False.");
+console.log(car !== 'subaru'); // False
+// Test 7
+console.log("Is car.length > 0? I predict True.");
+console.log(car.length > 0); // True
+// Test 8
+console.log("Is car.length < 5? I predict False.");
+console.log(car.length < 5); // False
+// Test 9
+console.log("Is car.startsWith('s')? I predict True.");
+console.log(car.startsWith('s')); // True
+// Test 10
+console.log("Is car.endsWith('u')? I predict True.");
+console.log(car.endsWith('u')); // True
